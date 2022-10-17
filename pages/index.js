617fff80f50f5a8263a32444c4b10acbb6d29543
JungleDiff29 <108928099+JungleDiff29@users.noreply.github.com>
2022-10-17 11:53:24 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import styles from "../styles/Home.module.css";
-import Login from './admin/index'
+import Bookborrow from './borrowers/index'
 
 export default function Home() {
   return (
     <div>
-     <Login/>
+     <Bookborrow/>
     </div>
   );
 }
